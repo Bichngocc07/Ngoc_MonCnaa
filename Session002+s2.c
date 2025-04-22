5d1be6d93626f1e48e7cc76cae5c190c0313e358
Bichngocc07 <ngoc01072006@gmail.com>
2025-04-22 20:52:07 +0700

    Add files via upload

---
+++ Session002+s2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Khai báo và khoi tao hai bien so nguyên
+    int a = 5;
+    int b = 10;
+    
+    // Tính tong hai so và luu vào mot bien khác
+    int sum = a + b;
+    
+    // Hien thi ket qua
+    printf("Tong cua %d và %d là %d\n", a, b, sum);
+
+    return 0;
+}
+
